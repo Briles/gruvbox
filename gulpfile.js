61be2555c79b696ec0026aab9bb5150c1cad5495
Brian Reilly <rilesdbrian@gmail.com>
2016-03-15 11:53:06 -0400

    Added support for fine-grained build control

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,18 @@ function execNode(command) {
 }
 
 gulp.task('build_theme', function () {
+  execNode(`${srcPath}build.js -t`);
+});
+
+gulp.task('build_scheme', function () {
+  execNode(`${srcPath}build.js -s`);
+});
+
+gulp.task('build_widget', function () {
+  execNode(`${srcPath}build.js -w`);
+});
+
+gulp.task('build_all', function () {
   execNode(`${srcPath}build.js`);
 });
 
@@ -27,21 +39,26 @@ gulp.task('build_icons', function () {
 });
 
 gulp.task('build', function (callback) {
-  runSequence('build_theme', 'build_icons', callback);
+  runSequence('build_all', 'build_icons', callback);
 });
 
 gulp.task('watch', function () {
   var themeFiles = [
-    `${srcPath}build.js`,
     `${srcPath}components.js`,
     `${srcPath}components/*.js`,
     `${srcPath}options.js`,
-    `${srcPath}palette.js`,
+    `${srcPath}paths.js`,
+    `${srcPath}sublime-options.js`,
+  ];
+
+  var schemeFiles = [
+    `${srcPath}scheme.js`,
+  ];
+
+  var widgetFiles = [
+    `${srcPath}options.js`,
     `${srcPath}paths.js`,
     `${srcPath}scheme.js`,
-    `${srcPath}sublime-options.js`,
-    `${srcPath}tinycolor.js`,
-    `${srcPath}utils.js`,
     `${srcPath}widget.js`,
   ];
 
@@ -51,8 +68,18 @@ gulp.task('watch', function () {
     `${srcPath}utils.js`,
   ];
 
-  gulp.watch(themeFiles, ['build_theme']);
+  var commonFiles = [
+    `${srcPath}build.js`,
+    `${srcPath}palette.js`,
+    `${srcPath}tinycolor.js`,
+    `${srcPath}utils.js`,
+  ];
+
+  gulp.watch(commonFiles, ['build_all']);
   gulp.watch(iconFiles, ['build_icons']);
+  gulp.watch(schemeFiles, ['build_scheme']);
+  gulp.watch(themeFiles, ['build_theme']);
+  gulp.watch(widgetFiles, ['build_widget']);
 });
 
 gulp.task('default', function (callback) {
--- src/build.js
+++ src/build.js
@@ -1,6 +1,14 @@
 (function () {
   'use strict';
 
+  const commander = require('commander');
+
+  commander
+    .option('-t, --theme', 'Build Themes')
+    .option('-s, --scheme', 'Build Color Schemes')
+    .option('-w, --widget', 'Build Widgets')
+    .parse(process.argv);
+
   const opts = require('./options.js');
 
   // Options
@@ -9,6 +17,10 @@
   const TEST_COLOR = opts.testColor || 'cyan';
   const BRIGHTNESS_MODES = ['Dark', 'Light'];
   const CONTRAST_MODES = ['Soft', 'Medium', 'Hard'];
+  const BUILD_MODE_THEME = !!commander.theme;
+  const BUILD_MODE_SCHEME = !!commander.scheme;
+  const BUILD_MODE_WIDGET = !!commander.widget;
+  const BUILD_MODE_ALL = !BUILD_MODE_THEME && !BUILD_MODE_SCHEME && !BUILD_MODE_WIDGET;
 
   const _ = require('lodash');
   _.mixin(require('lodash-deep'));
@@ -71,27 +83,29 @@
        * Themes
        */
 
-      const themeValues = {
-        colors: _.deepMapValues(entirePalette, c => tinycolor(c).toSublimeRgb()),
-        info: info,
-        paths: paths.internal,
-        options: sublimeOpts,
-      };
+      if (BUILD_MODE_THEME || BUILD_MODE_ALL) {
+        const themeValues = {
+          colors: _.deepMapValues(entirePalette, c => tinycolor(c).toSublimeRgb()),
+          info: info,
+          paths: paths.internal,
+          options: sublimeOpts,
+        };
 
-      var themeContents = [];
-      const themePath = path.join(paths.external.root, `${info.name}.sublime-theme`);
+        var themeContents = [];
+        const themePath = path.join(paths.external.root, `${info.name}.sublime-theme`);
 
-      components.forEach(function (c) {
-        const modulePath = path.join(paths.external.components, c);
-        const m = require(modulePath)(themeValues);
-        themeContents.push(m);
-      });
+        components.forEach(function (c) {
+          const modulePath = path.join(paths.external.components, c);
+          const m = require(modulePath)(themeValues);
+          themeContents.push(m);
+        });
 
-      themeContents = _.flatten(themeContents);
+        themeContents = _.flatten(themeContents);
 
-      var themeStringified = JSON.stringify(themeContents, null, JSON_WHITESPACE);
+        var themeStringified = JSON.stringify(themeContents, null, JSON_WHITESPACE);
 
-      utils.writeOutput(themePath, themeStringified);
+        utils.writeOutput(themePath, themeStringified);
+      }
 
       /**
        * Color Schemes
@@ -103,22 +117,26 @@
         paths: paths.internal,
       };
 
-      const schemeContents = require('./scheme.js')(schemeValues);
-      const schemeDestination = path.join(paths.external.root, `${info.name}.tmTheme`);
+      if (BUILD_MODE_SCHEME || BUILD_MODE_ALL) {
 
-      utils.writeOutput(schemeDestination, schemeContents);
+        const schemeContents = require('./scheme.js')(schemeValues);
+        const schemeDestination = path.join(paths.external.root, `${info.name}.tmTheme`);
+
+        utils.writeOutput(schemeDestination, schemeContents);
+      }
 
       /**
        * Widgets
        */
+      if (BUILD_MODE_WIDGET || BUILD_MODE_ALL) {
+        const widgetTemplate = require('./widget.js')(schemeValues);
 
-      const widgetTemplate = require('./widget.js')(schemeValues);
+        const widgetFilename = `Widget - ${info.name}.sublime-settings`;
+        const widgetDestination = path.join(paths.external.widgets, widgetFilename);
+        const widgetContents = JSON.stringify(widgetTemplate.settings, null, JSON_WHITESPACE);
 
-      const widgetFilename = `Widget - ${info.name}.sublime-settings`;
-      const widgetDestination = path.join(paths.external.widgets, widgetFilename);
-      const widgetContents = JSON.stringify(widgetTemplate.settings, null, JSON_WHITESPACE);
-
-      utils.writeOutput(widgetDestination, widgetContents);
+        utils.writeOutput(widgetDestination, widgetContents);
+      }
     });
   });
 
