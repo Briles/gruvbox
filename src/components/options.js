da3b7a5ad55b230362e6c6f4287473993507e1ad
Brian Reilly <rilesdbrian@gmail.com>
2016-03-16 13:06:17 -0400

    Added option to adjust command palette padding

--- src/components/options.js
+++ src/components/options.js
@@ -321,6 +321,58 @@ module.exports = function (values) {
     },
 
     /**
+     * Command Palette Padding
+     */
+
+    // Cramped Command Palette
+    {
+      'class': 'quick_panel',
+      'parents': [
+        {
+          'class': 'overlay_control',
+        },
+      ],
+      'settings': [stOpts.commandPaletteSpacingXs],
+      'row_padding': [14, 4],
+    },
+
+    // Compact Command Palette
+    {
+      'class': 'quick_panel',
+      'parents': [
+        {
+          'class': 'overlay_control',
+        },
+      ],
+      'settings': [stOpts.commandPaletteSpacingS],
+      'row_padding': [14, 8],
+    },
+
+    // Comfortable Command Palette
+    {
+      'class': 'quick_panel',
+      'parents': [
+        {
+          'class': 'overlay_control',
+        },
+      ],
+      'settings': [stOpts.commandPaletteSpacingL],
+      'row_padding': [14, 14],
+    },
+
+    // Cozy Command Palette
+    {
+      'class': 'quick_panel',
+      'parents': [
+        {
+          'class': 'overlay_control',
+        },
+      ],
+      'settings': [stOpts.commandPaletteSpacingXl],
+      'row_padding': [14, 16],
+    },
+
+    /**
      * Enable Status Bar Close Button
      */
     {
