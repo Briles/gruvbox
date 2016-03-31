e625c7f09b35a75b2c05579fa51d068a83638eda
Brian Reilly <rilesdbrian@gmail.com>
2016-03-31 12:19:07 -0400

    Improved configuration handling

--- src/js/controllers/configurator.js
+++ src/js/controllers/configurator.js
@@ -1,11 +1,28 @@
 module.exports = function ($scope, $routeParams, $location) {
   'use strict';
 
-  $scope.brightness = 'dark';
-  $scope.contrast = 'medium';
-  $scope.accent = '';
-  $scope.accentMod = '';
   $scope.location = $location.$$path;
+
+  $scope.conf = {
+    brightness: 'dark',
+    contrast: 'medium',
+    accent: '',
+    accentMod: '',
+  };
+
+  var getConfiguration = function () {
+    var currentConfiguration = [];
+
+    for (var option in $scope.conf) {
+      currentConfiguration.push($scope.conf[option]);
+    }
+
+    return currentConfiguration;
+  };
+
+  $scope.getClassList = function () {
+    return getConfiguration().join(' ');
+  };
 
   $scope.getTheme = function () {
     return $scope.brightness + $scope.contrast;
