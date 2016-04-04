0834605ca4f7eecc166d6591152c8d612110f055
Brian Reilly <rilesdbrian@gmail.com>
2016-04-03 23:05:54 -0400

    Added option to change button font sizes

--- src/components/options.js
+++ src/components/options.js
@@ -1941,5 +1941,107 @@ module.exports = function (values) {
       'font.size': 16,
     },
 
+    /**
+     * Button Font Sizes
+     */
+
+    // Extra Extra Small
+    {
+      class: 'label_control',
+      settings: [stOpts.buttonsFontSizeXxs],
+
+      parents: [
+
+        {
+          class: 'button_control',
+        },
+
+      ],
+
+      'font.size': 10,
+    },
+
+    // Default: 11
+
+    // Extra Small
+    {
+      class: 'label_control',
+      settings: [stOpts.buttonsFontSizeXs],
+
+      parents: [
+
+        {
+          class: 'button_control',
+        },
+
+      ],
+
+      'font.size': 12,
+    },
+
+    // Small
+    {
+      class: 'label_control',
+      settings: [stOpts.buttonsFontSizeS],
+
+      parents: [
+
+        {
+          class: 'button_control',
+        },
+
+      ],
+
+      'font.size': 13,
+    },
+
+    // Large
+    {
+      class: 'label_control',
+      settings: [stOpts.buttonsFontSizeL],
+
+      parents: [
+
+        {
+          class: 'button_control',
+        },
+
+      ],
+
+      'font.size': 14,
+    },
+
+    // Extra Large
+    {
+      class: 'label_control',
+      settings: [stOpts.buttonsFontSizeXl],
+
+      parents: [
+
+        {
+          class: 'button_control',
+        },
+
+      ],
+
+      'font.size': 15,
+    },
+
+    // Extra Extra Large
+    {
+      class: 'label_control',
+      settings: [stOpts.buttonsFontSizeXxl],
+
+      parents: [
+
+        {
+          class: 'button_control',
+        },
+
+      ],
+
+      'font.size': 16,
+    },
+
   ];
 };
