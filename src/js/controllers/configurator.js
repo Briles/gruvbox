3c4df9f3b9ea77f7a8e1189a97cb9d382c14b536
Brian Reilly <rilesdbrian@gmail.com>
2016-04-04 13:12:48 -0400

    Added command palette row spacing configurator

--- src/js/controllers/configurator.js
+++ src/js/controllers/configurator.js
@@ -18,6 +18,7 @@ module.exports = function ($scope, $routeParams, $location) {
     buttonsFontSize: '',
     statusbarFontSize: '',
     statusbarButtonWidth: '',
+    commandPaletteSpacing: '',
   };
 
   var getConfiguration = function () {
