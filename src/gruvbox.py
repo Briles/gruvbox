253422c59fc361e821b351edb660ad618d8bf0c1
akshaythakare7 <32819562+akshaythakare7@users.noreply.github.com>
2017-10-16 20:10:24 +0530

    Added GruvboxSelect class

---
+++ src/gruvbox.py
@@ -0,0 +1,62 @@
+import math
+import sublime
+import sublime_plugin
+
+class GruvboxSelect(sublime_plugin.TextCommand):
+    def run(self, action):
+        color_schemes = sublime.find_resources("gruvbox*.tmTheme")
+        color_themes = sublime.find_resources("gruvbox*.sublime-theme")
+
+        temp_schemes = []
+        self.themes = []
+        self.schemes = []
+
+        for scheme in color_schemes:
+            if 'Packages/gruvbox/' in scheme:
+                temp_schemes.append(scheme[17:-8])
+
+        for i in range(len(temp_schemes)):
+            if (i % 2) == 0:
+                self.schemes.insert(i + 1, temp_schemes[i])
+            else:
+                self.schemes.insert(i - 1, temp_schemes[i])
+
+        for theme in color_themes:
+            if 'Packages/gruvbox/' in theme:
+                self.themes.append(theme[17:])
+
+        self.show_panel()
+
+    def show_panel(self):
+        self.view.window().show_quick_panel(self.schemes, self.on_done, on_highlight=self.on_highlighted)
+
+    def on_done(self, index):
+        self.set_scheme('Packages/gruvbox/' + self.schemes[index] + '.tmTheme')
+        self.set_theme(self.themes[self.find_index(index)])
+        self.save_settings(self.schemes[index])
+
+    def on_highlighted(self, index):
+        self.set_scheme('Packages/gruvbox/' + self.schemes[index] + '.tmTheme')
+        self.set_theme(self.themes[self.find_index(index)])
+
+    def find_index(self, index):
+        if index == 0 or index == 1:
+            return 0
+        else:
+            return int(math.floor((index / 2)))
+
+    def set_scheme(self, scheme):
+        self.get_settings().set('color_scheme', scheme)
+
+    def set_theme(self, theme):
+        self.get_settings().set('theme', theme)
+
+    def get_settings(self):
+        return sublime.load_settings('Preferences.sublime-settings')
+
+    def save_settings(self, theme):
+        sublime.save_settings('Preferences.sublime-settings')
+        sublime.status_message('gruvbox: ' + theme)
+        print('')
+        print('[gruvbox] ' +  theme)
+        print('')
