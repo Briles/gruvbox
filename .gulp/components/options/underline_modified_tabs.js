f1d1f128cf07eecc1da3b2b5101c78aec682bd46
Brian Reilly <Briles@users.noreply.github.com>
2019-04-13 23:48:50 -0400

    support highlight_modified_tabs setting

--- .gulp/components/options/underline_modified_tabs.js
+++ .gulp/components/options/underline_modified_tabs.js
@@ -39,5 +39,41 @@ module.exports = function (values) {
 
       'layer2.inner_margin': [0, 0, 0, 2],
     },
+
+    /* support for highlight_modified_tabs setting */
+
+    // Default Thickness
+    ...mixins.createComponentVariations((lumin, palette) => {
+      return {
+        class: 'tab_control',
+        settings: ['highlight_modified_tabs'],
+        attributes: ['dirty'],
+
+        parents: [
+          {
+            attributes: [lumin],
+          },
+        ],
+
+        'layer2.tint': palette('gray'),
+      };
+    }),
+
+    {
+      class: 'tab_control',
+      settings: ['highlight_modified_tabs'],
+      attributes: ['dirty', 'selected'],
+
+      'layer2.opacity': 1, // Border - Bottom
+    },
+
+    // Thick Thickness
+    {
+      class: 'tab_control',
+      settings: ['highlight_modified_tabs', stOpts.underlineDirtyTabsThick],
+      attributes: ['dirty'],
+
+      'layer2.inner_margin': [0, 0, 0, 2],
+    },
   ];
 };
